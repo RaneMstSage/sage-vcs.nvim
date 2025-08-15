4873aa0656a1462ffd71ad35009291666e406163
Kenneth Schuetz <rane@mstsage.com>
2025-08-15 17:16:36 -0400

    fixed syntax error of line.match which should have been line:match

--- lua/sage-vcs/svn.lua
+++ lua/sage-vcs/svn.lua
@@ -45,7 +45,7 @@ function M.get_svn_info()
     if handle then
         for line in handle:lines() do
             if line:match('^URL:') then
-                result.url = line.match('^URL:%s*(.+)')
+                result.url = line:match('^URL:%s*(.+)')
             elseif line:match('^Repository Root:') then
                 result.root = line:match('^Repository Root:%s*(.+)')
             elseif line:match('^Relative URL:') then
